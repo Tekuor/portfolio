450468507765525d0964cf5486c46971dfb83059
Matekuor <51318778+Tekuor@users.noreply.github.com>
2021-12-13 08:27:49 +0000

    modified modal

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -59,7 +59,7 @@
             <div class="md:mt-5 mt-4">
               <p class="audio-text">
                 <span style="color: #D65CD6; font-weight: bold">Listen</span>
-                to how 12+ others butchered my name
+                to how others butchered my name
               </p>
             </div>
           </div>
@@ -69,13 +69,14 @@
       <modal
         name="example"
         width="100%"
-        height="100%"
+        height="auto"
         :clickToClose="true"
         :showClose="true"
+        :scrollable="true"
       >
         <div
           style="color:black; background-color: #3a2f52;"
-          class="w-full h-full"
+          class="w-full h-screen"
         >
           <div class="flex flex-col items-end pt-8 pr-12">
             <button id="hamburger" @click="closeModal()">
@@ -87,9 +88,34 @@
               />
             </button>
           </div>
-          <div class="flex flex-col items-center mt-8">
+          <div v-if="showRecordings" class="flex flex-col items-center mt-8">
+            <div class="mb-8">
+              <p class="recording-text text-lg">
+                Listen to how others butchered my name
+              </p>
+              <p
+                class="listen-text cursor-pointer"
+                @click="showRecordings = false"
+              >
+                Record yourself pronouncing my name
+              </p>
+            </div>
+            <div class="recordings-div flex flex-col items-center pt-8">
+              <audio
+                controls
+                :src="recording.secure_url"
+                :key="index"
+                class="mb-8"
+                v-for="(recording, index) in recordedFiles"
+              >
+                Your browser does not support the
+                <code>audio</code> element.
+              </audio>
+            </div>
+          </div>
+          <div v-else class="flex flex-col items-center mt-8">
             <div class="flex flex-row mb-8" v-if="!file">
-              <div v-show="!recording">
+              <div v-show="!recording && !saved && !discarded">
                 <button
                   class="rounded-full h-14 w-14 flex flex-col items-center"
                   style="background: rgba(255, 255, 255, 0.1);"
@@ -105,27 +131,105 @@
                   class="rounded-full h-14 w-14 flex flex-col items-center"
                   style="background: rgba(255, 255, 255, 0.1);"
                 >
-                  <img class="w-9 mt-2" src="@/assets/stop.png" />
+                  <img class="w-9 mt-3" src="@/assets/stopIcon.png" />
+                </button>
+              </div>
+
+              <div v-show="saved">
+                <button
+                  id="save"
+                  class="rounded-full h-14 w-14 flex flex-col items-center"
+                  style="background: rgba(255, 255, 255, 0.1);"
+                >
+                  <img class="w-9 mt-3" src="@/assets/check.png" />
+                </button>
+              </div>
+
+              <div v-show="discarded">
+                <button
+                  id="discard"
+                  class="rounded-full h-14 w-14 flex flex-col items-center"
+                  style="background: rgba(255, 255, 255, 0.1);"
+                >
+                  <img class="w-7 mt-3" src="@/assets/discard.png" />
                 </button>
               </div>
             </div>
 
-            <audio v-else controls :src="file">
-              Your browser does not support the
-              <code>audio</code> element.
-            </audio>
+            <div
+              v-show="recorded && !saved && !discarded"
+              class="flex flex-col items-center"
+            >
+              <audio controls :src="file">
+                Your browser does not support the
+                <code>audio</code> element.
+              </audio>
+
+              <div class="flex flex-row mt-8">
+                <button
+                  class="discard-btn px-4 text-white font-semibold"
+                  type="button"
+                  @click="discardAudio()"
+                >
+                  Discard
+                </button>
+                <button
+                  class="ml-6 save-btn px-4 text-white font-semibold"
+                  type="button"
+                  @click="saveAudio()"
+                >
+                  Save
+                </button>
+              </div>
+            </div>
 
             <div class="flex flex-col items-center" v-if="!file">
-              <div class="recording-heading">
-                Click on the mic Icon to Start recording
+              <div v-show="recording" class="time-text mb-2">
+                {{ duration }}s
               </div>
-              <div class="recording-text">Record you pronounce my name 😆</div>
+
+              <div
+                v-if="!recording && !saved && !discarded"
+                class="recording-heading mb-2 cursor-pointer"
+                @click="record()"
+              >
+                Start recording
+              </div>
+              <div
+                v-else-if="!recording && saved"
+                class="recording-heading mb-2 cursor-pointer"
+              >
+                Recording Saved
+              </div>
+              <div
+                v-else-if="!recording && discarded"
+                class="recording-heading mb-2 cursor-pointer"
+              >
+                Recording Discarded
+              </div>
+              <div v-else class="recording-heading mb-2 cursor-pointer">
+                Stop recording
+              </div>
+
+              <div
+                class="recording-text"
+                v-if="!recording && !saved && !discarded"
+              >
+                Record yourself pronounce my name 😆
+              </div>
+            </div>
+
+            <div
+              class="listen-text mt-10 cursor-pointer"
+              @click="getRecordings()"
+            >
+              Listen to how others butchered my name >
             </div>
 
             <div class="name-div min-h-96 mt-8 p-10">
               <div class="flex flex-row">
                 <div
-                  class="rounded-full h-8 w-8"
+                  class="rounded-full h-8 w-9 cursor-pointer"
                   style="background: rgba(255, 255, 255, 0.1);"
                   @click="playSound(require('../assets/Matekuor.mp3'))"
                 >
@@ -157,12 +261,21 @@
 </template>
 
 <script>
+import { format } from "date-fns";
+
 export default {
   data() {
     return {
       file: "",
       preset: "kosvy4q2",
       recording: false,
+      duration: 0,
+      saved: false,
+      discarded: false,
+      recorded: false,
+      formData: {},
+      recordedFiles: [],
+      showRecordings: false,
     };
   },
   methods: {
@@ -170,6 +283,7 @@ export default {
       this.$modal.show("example");
     },
     closeModal() {
+      this.reset();
       this.$modal.hide("example");
     },
     playSound(sound) {
@@ -178,11 +292,22 @@ export default {
         audio.play();
       }
     },
+    reset() {
+      this.saved = false;
+      this.discarded = false;
+      this.recorded = false;
+      this.recording = false;
+    },
     record() {
       navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
         this.recording = true;
         const mediaRecorder = new MediaRecorder(stream);
+        let start = Date.now();
         mediaRecorder.start();
+
+        let timer = setInterval(() => {
+          this.duration = format(Date.now() - start, "mm:ss");
+        }, 1000);
 
         const audioChunks = [];
 
@@ -190,33 +315,71 @@ export default {
           audioChunks.push(event.data);
         });
 
+        this.formData = new FormData();
+
         mediaRecorder.addEventListener("stop", () => {
           this.recording = false;
+          clearInterval(timer);
           const audioBlob = new Blob(audioChunks);
           const audioUrl = URL.createObjectURL(audioBlob);
           this.file = audioUrl;
 
-          let formData = new FormData();
-          formData.append("upload_preset", this.preset);
-          formData.append("file", audioBlob);
+          this.formData.append("folder", "portfolio");
+          this.formData.append("tags", "portfolio");
+          this.formData.append("resource_type", "video");
+          this.formData.append("access_mode", "authenticated");
+          this.formData.append("file", audioBlob);
 
-          fetch(`https://api.cloudinary.com/v1_1/dt7xfykxp/upload`, {
-            method: "POST",
-            body: formData,
-          })
-            .then((response) => response.json())
-            .then((result) => {
-              console.log("Success:", result);
-            })
-            .catch((error) => {
-              console.error("Error:", error);
-            });
+          this.recording = false;
+          this.recorded = true;
         });
 
         document.getElementById("stop").addEventListener("click", function() {
           mediaRecorder.stop();
         });
       });
+    },
+    saveAudio() {
+      fetch(`http://localhost:8000/upload`, {
+        method: "POST",
+        body: this.formData,
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          console.log("Success:", result);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        })
+        .finally(() => {
+          this.recording = false;
+          this.saved = true;
+          this.file = "";
+        });
+    },
+    getRecordings() {
+      fetch(`http://localhost:8000/get-files`, {
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          console.log("Success:", result.response.resources);
+          this.recordedFiles = result.response.resources;
+          this.showRecordings = true;
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        })
+        .finally(() => {
+          // this.recording = false;
+          // this.saved = true;
+          // this.file = "";
+        });
+    },
+    discardAudio() {
+      this.recording = false;
+      this.discarded = true;
+      this.file = "";
     },
   },
 };
@@ -227,7 +390,6 @@ export default {
 .first-text {
   font-style: normal;
   font-weight: 600;
-  /* font-size: 64px; */
   color: #ffffff;
 }
 
@@ -267,7 +429,7 @@ export default {
 .recording-heading {
   font-style: normal;
   font-weight: bold;
-  font-size: 16px;
+  font-size: 20px;
   line-height: 19px;
   text-align: center;
   color: #ffffff;
@@ -302,6 +464,25 @@ export default {
   color: #ffffff;
 }
 
+.time-text {
+  font-weight: 300;
+  font-size: 16px;
+  text-align: center;
+  color: #d65cd6;
+}
+
+.discard-btn {
+  height: 43px;
+  background: #d65cd6;
+  border-radius: 8px;
+}
+
+.save-btn {
+  height: 43px;
+  background: #9e71fc;
+  border-radius: 8px;
+}
+
 @media only screen and (max-width: 600px) {
   .audio-text {
     font-size: 12px;
@@ -317,7 +498,22 @@ export default {
   font-size: 16px;
 }
 
+.listen-text {
+  font-weight: normal;
+  font-size: 12px;
+  text-align: center;
+  color: #d65cd6;
+}
+
 audio::-webkit-media-controls-panel {
   /* background-color: #56AEFF; */
+}
+
+.recordings-div {
+  position: static;
+  width: 406px;
+  max-height: 544px;
+  background: rgba(255, 255, 255, 0.1);
+  border-radius: 16px;
 }
 </style>
