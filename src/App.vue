fe41dc1f01865d2857680b985d82a1ad8af22fc3
Matekuor <matekuor94@gmail.com>
2022-02-09 22:01:16 +0000

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,58 @@
 <template>
-  <div id="app" class="h-full bg-green-400">
+  <div id="app" class="h-full">
     <TopNav @goto="goto" />
     <HeroSection id="hero" class="mb-20" />
     <Journey id="journey" @goto="goto" class="mb-20" />
     <Projects id="projects" class="mb-20" />
     <Stack id="stack" class="mb-20" />
     <Contact id="contact" class="pb-40" />
+
+    <footer class="w-full py-10 text-white">
+      <div class="flex flex-row h-auto justify-between px-12">
+        <div>
+          <h2 class="text-lg font-semibold">Matekuor Mate-Kole</h2>
+          <p class="text-gray-300">Fullstack developer</p>
+        </div>
+        <div>
+          <h2 class="text-lg font-semibold">Site Map</h2>
+          <p class="text-gray-300 cursor-pointer pt-4" @click="goto('hero')">
+            Home
+          </p>
+          <p class="text-gray-300 cursor-pointer pt-4" @click="goto('journey')">
+            About
+          </p>
+          <p class="text-gray-300 cursor-pointer pt-4" @click="goto('stack')">
+            Stack
+          </p>
+          <p
+            class="text-gray-300 cursor-pointer pt-4"
+            @click="goto('projects')"
+          >
+            Projects
+          </p>
+          <p class="text-gray-300 cursor-pointer pt-4" @click="goto('contact')">
+            Contact
+          </p>
+        </div>
+        <div class="flex flex-col">
+          <h2 class="text-lg font-semibold">Social</h2>
+          <a
+            href="https://www.linkedin.com/in/giftymatekole/"
+            target="blank"
+            class="text-gray-300 cursor-pointer pt-4"
+            >LinkedIn</a
+          >
+          <a
+            href="https://github.com/Tekuor"
+            target="blank"
+            class="text-gray-300 cursor-pointer pt-4"
+            >Github</a
+          >
+        </div>
+      </div>
+    </footer>
+
+    <notifications classes="my-notification" />
   </div>
 </template>
 
@@ -47,5 +94,19 @@ export default {
   background: #3a2f52;
   color: rgba(255, 255, 255, 0.1);
   width: 100%;
+}
+
+footer {
+  position: absolute;
+  min-height: 116px;
+  background: #9e71fc;
+}
+
+.my-notification {
+  margin: 0 5px 5px;
+  padding: 10px;
+  font-size: 12px;
+  color: #ffffff;
+  background: #d65cd6 !important;
 }
 </style>
