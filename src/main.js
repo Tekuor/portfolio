9b5f7f06e9564935c1d86366fdc34b9b00fe7784
Matekuor <matekuor94@gmail.com>
2022-02-09 22:01:41 +0000

    Added vue-notification

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./index.css";
 import VModal from "vue-js-modal";
+import Notifications from "vue-notification";
 
 Vue.config.productionTip = false;
 Vue.use(VModal);
+Vue.use(Notifications);
 
 new Vue({
   render: (h) => h(App),
